3c473b3095c2d99a8884861a5e471d3dff1cdecd
mgoldchild <mgoldchild@gmail.com>
2015-08-13 19:34:04 +0900

    auto commit

--- .mongorc.js
+++
@@ -1,6 +0,0 @@
-DBCollection.prototype.findOneById = DBCollection.prototype.fbi = function(id)
-{
-        if (!id) return 'idを入れてください';
-        if (typeof id !== 'string') id = String(id);
-        return this.findOne({_id:ObjectId(id)});
-};
