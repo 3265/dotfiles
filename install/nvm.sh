11ac76e3db79b8d65c8862013c265b2faf88b40e
mgoldchild <mgoldchild@gmail.com>
2015-08-18 19:23:43 +0900

    modified nvm

--- install/nvm.sh
+++ install/nvm.sh
@@ -1,7 +1,5 @@
-
 git clone git://github.com/creationix/nvm.git ~/.nvm
-
+source ~/.nvm/nvm.sh
 nvm install v0.12.00
-nvm use v0.12.0
-nvm alias default v0.12.0
-node -version
+nvm alias default v0.12.00 default
+nvm use default
