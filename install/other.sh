00da286d27162776c2ae59f04722ed0d87332af5
mgoldchild <mgoldchild@gmail.com>
2015-10-04 00:22:49 +0900

    Modified editconfig

--- install/other.sh
+++ install/other.sh
@@ -1,5 +1,8 @@
-# peco install ubuntu
+# peco install in ubuntu
 curl https://github.com/peco/peco/releases/download/v0.3.2/peco_linux_amd64.tar.gz
 tar xfvz peco_linux_amd64.tar.gz
 sudo cp peco_linux_amd64/peco /user/bin/
 sudo chmod 777 peco
+
+# windows install list
+# putty, poderosa, winscp, winsshfs, sftp net drive, gitbash
