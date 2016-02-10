58c59f65e9f786776d4f5ffb6480559bf6170b2e
mgoldchild <mgoldchild@gmail.com>
2016-02-10 17:37:53 +0900

    色々変更

--- setup/mint.sh
+++ setup/mint.sh
@@ -9,32 +9,17 @@ sudo apt-get install firefox
 sudo apt-get install cinnamon-desktop-environment
 
 # essentials
-sudo apt-get install vim
-sudo apt-get install screen
-sudo apt-get install ssh
-sudo apt-get install mosh
-sudo apt-get install git
-sudo apt-get install bash-completion
-sudo apt-get install curl
-sudo apt-get install sshfs
-sudo apt-get install ibus-mozc
-sudo apt-get install silversearcher-ag
-sudo apt-get install jq
-sudo apt-get install openjdk-7-jdk
-
-# Nemo
-# sudo apt-get install nemo
-# sudo apt-get install nemo-compare
-# sudo apt-get install nemo-dropbox
-# sudo apt-get install nemo-fileroller
-# sudo apt-get install nemo-pastebin
-# sudo apt-get install nemo-seahorse
-# sudo apt-get install nemo-share
-# sudo apt-get install nemo-preview
-# sudo apt-get install nemo-rabbitvcs
-# sudo apt-get install nemo-media-columns
-# sudo apt-get install nemo-emblems
-# sudo apt-get install nemo-image-converter
-# sudo apt-get install nemo-audio-tab
-
+sudo apt-get install -y vim
+sudo apt-get install -y screen
+sudo apt-get install -y ssh
+sudo apt-get install -y mosh
+sudo apt-get install -y git
+sudo apt-get install -y bash-completion
+sudo apt-get install -y curl
+sudo apt-get install -y sshfs
+#sudo apt-get install -y ibus-mozc
+sudo apt-get install -y silversearcher-ag
+sudo apt-get install -y jq
+sudo apt-get install -y nodejs
+sudo apt-get install -y openjdk-7-jdk
 
--- setup/nvm.sh
+++ setup/nvm.sh
@@ -2,6 +2,6 @@
 
 git clone git://github.com/creationix/nvm.git ~/.nvm
 source ~/.nvm/nvm.sh
-nvm install v0.12.00
-nvm alias default v0.12.00 default
+nvm install v0.12.0
+nvm alias default v0.12.0 default
 nvm use default
