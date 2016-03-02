fd3084c8a8bae393b1d5e29dcd2e5205e85c3e5c
mgoldchild <mgoldchild@gmail.com>
2016-03-02 19:04:03 +0900

    色々追加

--- index.js
+++
@@ -1,4 +0,0 @@
-require('http').createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello World');
-}).listen(80);
--- install/go.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-go get github.com/peco/peco/cmd/peco
-go get github.com/github/hub
-
-go get github.com/monochromegane/the_platinum_searcher
-
