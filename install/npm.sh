2f825d5e773eea23e3b0e354b42e7460b090eb22
mgoldchild <mgoldchild@gmail.com>
2016-04-07 16:49:23 +0900

    modified npm.sh

--- install/npm.sh
+++ install/npm.sh
@@ -5,20 +5,17 @@ npm install -g json
 # build tool
 npm instlal -g node-gyp
 #sudo apt-get install lib32stdc++6 lib32z1 lib32z1-dev #for enclosejs
-npm install -g enclose
+# npm install -g enclose
 
 # doc tool
 npm install -g jsdoc
-npm install -g documentation
-npm install -g hackmyresume
-
-# for repl
-npm install -g underscore
+# npm install -g documentation
+# npm install -g hackmyresume
 
 # package manager
 npm install -g bower
-npm install -g meteorite
-npm install -g npm-install-missing
+# npm install -g meteorite
+# npm install -g npm-install-missing
 
 # lint tool
 npm install -g jshint
@@ -26,56 +23,56 @@ npm install -g eslint
 npm install -g coffeelint
 
 # development tool
-npm install -g express
-#npm install -g meteor
-npm install -g express-generator
-npm install -g pm2
-npm install -g browserify
-npm install -g mocha
 npm install -g gulp
-npm install -g grunt-cli
-npm install -g yeoman
-npm install -g yo
-npm install -g nodedev
-npm install -g nodemon
+# npm install -g express
+# npm install -g meteor
+# npm install -g express-generator
+# npm install -g pm2
+# npm install -g browserify
+# npm install -g mocha
+# npm install -g grunt-cli
+# npm install -g yeoman
+# npm install -g yo
+# npm install -g nodedev
+# npm install -g nodemon
 
 # other tool
-npm install -g readme-gen
-npm install -g azure-cli
 npm install -g mongo-hacker
-npm install -g node-inspector
-npm install -g nmv
-npm install -g vtop
-npm install -g plato
-npm install -g git-stats
-npm install -g nyancat
-npm install -j asciify
-npm install -g hell
-npm install -g issues
-npm install -g todo
-npm install -g js2coffee
+# npm install -g readme-gen
+# npm install -g azure-cli
+# npm install -g node-inspector
+# npm install -g nmv
+# npm install -g vtop
+# npm install -g plato
+# npm install -g git-stats
+# npm install -g nyancat
+# npm install -j asciify
+# npm install -g hell
+# npm install -g issues
+# npm install -g todo
+# npm install -g js2coffee
 
 # alt js
-npm install -g babel
 npm install -g coffee-script
-npm install -g cirru-script
-npm install -g typescript
-npm install -g livescript
-npm install -g purescript
-npm install -g nativescript
-npm install -g sweet.js
-npm install -g jsx
+# npm install -g babel
+# npm install -g cirru-script
+# npm install -g typescript
+# npm install -g livescript
+# npm install -g purescript
+# npm install -g nativescript
+# npm install -g sweet.js
+# npm install -g jsx
 
 # alt css
-npm install -g less
-npm install -g stylus
+# npm install -g less
+# npm install -g stylus
 
 # IDE
-npm install -g nuclide
-npm install -g slap
+# npm install -g nuclide
+# npm install -g slap
 
 # news
 #npm install -g @rafaelrinaldi/hn-cli
 
 # others
-npm install -g gitbook
+# npm install -g gitbook
