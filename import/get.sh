9c5f7cbc576c6afefd10104a64a96fc85824e8f7
mgoldchild <mgoldchild@gmail.com>
2017-01-06 19:09:02 +0900

    CompileDeamenを追加

--- import/get.sh
+++ import/get.sh
@@ -13,7 +13,9 @@ go get github.com/peco/peco/cmd/peco
 go get github.com/github/hub
 go get github.com/monochromegane/the_platinum_searcher
 
-# Stock market tracker
-go get github.com/michaeldv/mop
-cd $GOPATH/src/github.com/michaeldv/mop && make install
+# Go REPL
+go get -u github.com/nsf/gocode
+
+# Auto Compiler
+go get -u github.com/githubnemo/CompileDaemon
 
