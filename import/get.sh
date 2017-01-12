ba3e4cb64b9c065f964b4f92d4b1e47e4e7aee77
mgoldchild <mgoldchild@gmail.com>
2017-01-13 00:10:11 +0900

    alias変更

--- import/get.sh
+++ import/get.sh
@@ -15,9 +15,6 @@ go get github.com/peco/peco/cmd/peco
 go get github.com/github/hub
 go get github.com/monochromegane/the_platinum_searcher
 
-# Go REPL
-go get -u github.com/nsf/gocode
-
 # Auto Compiler
 go get -u github.com/githubnemo/CompileDaemon
 
