12acae14df01967297f266d28f594ca86160e923
mgoldchild <mgoldchild@gmail.com>
2017-05-31 12:53:26 +0900

    atomも少し

--- setup/apm.sh
+++ setup/apm.sh
@@ -5,35 +5,39 @@
 # ######################
 
 # 必須
-apm install japanese-menu
-apm install file-icons
-apm install highlight-column
-apm install highlight-line
-apm install vim-mode
+# apm install japanese-menu
+# apm install file-icons
+# apm install highlight-column
+# apm install highlight-line
+apm install vim-mode-plus
+apm install vim-mode-plus-ex-mode
 
 #apm install atom-alignmen
-apm install tag
-apm install highlight-selected
-apm install script
+# apm install tag
+# apm install highlight-selected
+# apm install script
 
 # 静的構文解析
 apm install linter
-apm install linter-jshint
-apm install linter-coffeelint
+apm install linter-eslint
+# apm install linter-jshint
+# apm install linter-coffeelint
 #apm install linter-shellcheck
 
+apm install react
+
 # 入力補完系
-apm install autocomplete-plus
-apm install autocomplete-snippets
-apm install docblockr
-apm install autocomplete-paths
+# apm install autocomplete-plus
+# apm install autocomplete-snippets
+# apm install docblockr
+# apm install autocomplete-paths
 
 # 見た目系
-apm install color-picker
-apm install pretty-json
-apm install atom-prettify
-apm install minimap
+# apm install color-picker
+# apm install pretty-json
+# apm install atom-prettify
+# apm install minimap
 
 # 補助系
-apm install merge-conflicts
-apm install travis-ci-status
+# apm install merge-conflicts
+# apm install travis-ci-status
