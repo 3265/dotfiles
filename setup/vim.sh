cb96c619f8afe623fda84528210a43264ada2743
mgoldchild <mgoldchild@gmail.com>
2018-06-26 19:55:44 +0900

    Add font.sh for terminal

--- setup/vim.sh
+++ setup/vim.sh
@@ -4,10 +4,3 @@
 cd
 mkdir -p ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/vundle.git
-
-# nerdtree icon dl
-cd
-git clone https://github.com/ryanoasis/nerd-fonts.git ~/nerd-fonts
-~/nerd-fonts/install.sh
-rm -rf ~/nerd-fonts
-
