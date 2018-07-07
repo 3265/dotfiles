75cd535f3fa344c723634033586a9286611331a3
mgoldchild <mgoldchild@gmail.com>
2018-07-07 22:37:23 +0800

    Add sh script for new workspace in i3

---
+++ i3/new_workspace.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Note: need to add permission
+# chmod +x ~/dotfiles/i3/empty_workspace.sh
+
+# ref https://faq.i3wm.org/question/6004/creating-a-new-workspace.1.html
+i3-msg workspace $(($(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1))
+i3-input -F 'rename workspace to "%s"' -P 'Rename workspace: '
+dmenu_run
+
+
