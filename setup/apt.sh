f723342406ae40004fe2bcc502638ca9b6c316e6
mgoldchild <mgoldchild@gmail.com>
2018-07-10 21:38:16 +0800

    updat apt.sh

--- setup/apt.sh
+++ setup/apt.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 
-# Neccesary
-# sudo apt-get install git -y
-# sudo apt-get install terminator -y
-
 # CUI
 sudo apt-get install zsh -y
 sudo apt-get install vim -y
-sudo apt-get install screen -y
-sudo apt-get install tmux -y
 sudo apt-get install silversearcher-ag -y
 sudo apt-get install ranger -y
-sudo apt-get install tig -y
 sudo apt-get install curl -y
 sudo apt-get install tree -y
+
+# For git
+sudo apt-get install tig -y
+sudo apt-get install git -y
+
+# Tmux & Screen
+sudo apt-get install tmux -y
+sudo apt-get install xsel -y # for yank
+sudo apt-get install screen -y
+
+# WM
+sudo apt-get install i3 -y
+sudo apt-get install feh -y # for wallpaper
+sudo apt-get install compton -y # for transparent
+sudo apt-get install parcellite -y # for clip board
+sudo apt-get install volumeicon-alsa -y # to use volumeicon
 sudo apt-get install neofetch -y # cli sys info tool
+sudo apt-get install moc moc-ffmpeg-plugin # for Music
+sudo apt install net-tools # for ifconfig(used by conky-info)
 
 # Programing Language
 sudo apt-get install golang -y
@@ -22,14 +33,7 @@ sudo apt-get install python-pip -y
 sudo apt-get install python3-pip -y
 
 # GUI
-sudo apt-get install i3 -y
-sudo apt-get install feh -y # for wallpaper
-sudo apt-get install compton -y # for transparent
-sudo apt-get install parcellite -y # for clip board
-sudo apt-get install volumeicon-alsa -y # to use volumeicon
-
-# Tmux
-sudo apt-get install xsel -y # for yank
+sudo apt-get install chromium-browser -y
 
 # Business
 sudo snap install slack --classic
