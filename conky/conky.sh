99cd7dd8489ec775fd5823856c437fac08306227
mgoldchild <mgoldchild@gmail.com>
2018-07-15 11:32:05 +0800

    Modify conky

---
+++ conky/conky.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+conky -d -c ~/dotfiles/conky/conky-sys
+conky -d -c ~/dotfiles/conky/conky-profile
+conky -d -c ~/dotfiles/conky/conky-time
+conky -d -c ~/dotfiles/conky/conky-cal
+conky -d -c ~/dotfiles/conky/conky-news
+conky -d -c ~/dotfiles/conky/conky-gcal
--- i3/conky.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-conky -d -c ~/dotfiles/i3/conky-sys
-conky -d -c ~/dotfiles/i3/conky-profile
-conky -d -c ~/dotfiles/i3/conky-time
-conky -d -c ~/dotfiles/i3/conky-cal
-conky -d -c ~/dotfiles/i3/conky-news
-conky -d -c ~/dotfiles/i3/conky-gcal
