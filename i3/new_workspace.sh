def3353a1117eb7b2132732e1f03f537c5ba0dde
mgoldchild <mgoldchild@gmail.com>
2018-07-19 01:25:36 +0800

    dmenu -> rofi

--- i3/new_workspace.sh
+++ i3/new_workspace.sh
@@ -6,6 +6,6 @@
 # ref https://faq.i3wm.org/question/6004/creating-a-new-workspace.1.html
 i3-msg workspace $(($(i3-msg -t get_workspaces | tr , '\n' | grep '"num":' | cut -d : -f 2 | sort -rn | head -1) + 1))
 # i3-input -F 'rename workspace to "%s"' -P 'Rename workspace: '
-dmenu_run
-
+# dmenu_run
+rofi -show run # usae rofi instead of dmenu
 
