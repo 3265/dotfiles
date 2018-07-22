dafdfb9f5dca5cd7a9565aebabb685cafa57111a
mgoldchild <mgoldchild@gmail.com>
2018-07-22 22:51:27 +0800

    bugfix

--- i3/check_btr.sh
+++ i3/check_btr.sh
@@ -2,8 +2,12 @@
 
 # NOTE: need to install acpitool
 BATTINFO=`acpitool -b`
-if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | tail -c 8` < 00:15:00 ]] ; then
-    MSG="Low Battery"
+CHARGING=`echo $BATTINFO | grep Discharging`
+REMAINING=`echo $BATTINFO | tail -c 8`
+DT1=`date -d "$REMAINING" '+%s'`
+DT2=`date -d "00:15:00" '+%s'`
+
+if [[ $CHARGING && $DT1 -lt  $DT2 ]] ; then
     # cronから実行するため環境変数DISPLAYをつける
-    DISPLAY=:0 notify-send -u critical "$MSG" "$BATTINFO"
+    DISPLAY=:0 notify-send -u critical "Low Battery" "$BATTINFO"
 fi
