da5776d9bb0bf4c626c0d9dd6e7896d7707962bf
mgoldchild <mgoldchild@gmail.com>
2018-08-12 19:05:17 +0800

    iroiro

--- pkg/get.sh
+++ pkg/get.sh
@@ -14,15 +14,17 @@ go get -u github.com/nsf/gocode
 go get -u golang.org/x/tools/cmd/goimports
 go get -u golang.org/x/tools/cmd/gorename
 
-# terminal tools
-go get github.com/peco/peco/cmd/peco
-go get -u github.com/gokcehan/lf
-go get github.com/mattn/memo
+# Essential
 go get github.com/knqyf263/pet
-go get github.com/asciimoo/wuzz
-go get github.com/schachmat/wego
-go get github.com/github/hub
+go get github.com/peco/peco/cmd/peco
+
+# terminal tools
+# go get -u github.com/gokcehan/lf
+# go get github.com/mattn/memo
+# go get github.com/asciimoo/wuzz
+# go get github.com/schachmat/wego
+# go get github.com/github/hub
 # go get github.com/monochromegane/the_platinum_searcher
 
 # for development
-go get -u github.com/githubnemo/CompileDaemon
+# go get -u github.com/githubnemo/CompileDaemon
