6354f54b09cb18be1eedce2f7a1de866f192de7a
mgoldchild <mgoldchild@gmail.com>
2018-09-18 21:00:44 +0800

    Fix err

--- i3/monitor.sh
+++ i3/monitor.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 xrandr --output LVDS-1 --output HDMI-1 --auto --above LVDS-1 # monitor position; need to check display name by using xrandr
-feh --bg-center ~/dotfiles/i3/wallpaper.jpeg --image-bg #000000 # wallpaper
+feh --bg-center ~/dotfiles/i3/wallpaper.jpeg --image-bg black # wallpaper
