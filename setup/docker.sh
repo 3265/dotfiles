40e771e22610a614694ec8e8dbab12ec7b7eebb0
mgoldchild <mgoldchild@gmail.com>
2019-01-20 01:29:08 +0900

    Add y option

--- setup/docker.sh
+++ setup/docker.sh
@@ -5,7 +5,7 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
 sudo apt update
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 
 # docker-compose
 # ref https://www.digitalocean.com/community/tutorials/how-to-install-docker-compose-on-ubuntu-18-04#conclusion
