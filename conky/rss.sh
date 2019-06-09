d915a5a5a1c136befe3cfb10baf814e616768aa3
mgoldchild <mgoldchild@gmail.com>
2019-06-09 15:06:57 +0900

    delete conky

--- conky/rss.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# NOTE:
-# - curl と xml2 のインスコが必要
-# - conkyのrssコマンドが改行してくれないため
-# - foldで頭数字で改行されるとsedで置き換えているところで空白詰められない気がする
-max_line=50;count=1
-curl -sL ${1} | xml2 | grep "item/title" | awk -F'=' '{print $2}' | head --line=${count} | nl -n ln | fold -s -${max_line} | sed '/^[^0-9]/s/^/       /g'
-
