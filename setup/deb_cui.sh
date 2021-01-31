a198388b419c5f6f0369c55572627050f54e7268
mgoldchild <mgoldchild@gmail.com>
2021-01-31 18:10:11 +0900

    update deb commands

--- setup/deb_cui.sh
+++ setup/deb_cui.sh
@@ -4,3 +4,10 @@ sudo apt install -y fzf
 sudo apt install -y autojump
 sudo apt install -y exa
 
+
+# NOTE:
+# - way to install fzf on jetson agx xavier.
+# - go to the https://launchpad.net/ubuntu/+source/fzf
+# - download arm binary. e.g. fzf_0.24.3-1_arm64.deb
+# - install it by `sudo dpkg -i ~/Download/fzf_0.24.3-1_arm64.deb`
+
