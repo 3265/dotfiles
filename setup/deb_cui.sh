567d72ca015f18966c1762894f2faf5f9554092c
mgoldchild <mgoldchild@gmail.com>
2021-02-03 03:22:08 +0900

    update

--- setup/deb_cui.sh
+++ setup/deb_cui.sh
@@ -1,6 +1,7 @@
 sudo apt install -y zsh
 sudo apt install -y silversearcher-ag
 sudo apt install -y fzf
+sudo apt install -y xsel # for copy command
 sudo apt install -y autojump
 sudo apt install -y exa
 
