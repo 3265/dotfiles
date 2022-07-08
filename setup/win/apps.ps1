2e2560efcd721ba1dad9574e579d99a619542565
mgoldchild <mgoldchild@gmail.com>
2022-07-08 13:31:42 +0900

    update win script

--- setup/win/apps.ps1
+++ setup/win/apps.ps1
@@ -11,6 +11,9 @@ winget install Lexikos.AutoHotkey
 winget install Greenshot.Greenshot
 winget install Microsoft.PowerToys # caps lock
 
+# Office
+# MS Office, OneNote, EdraMax
+
 # Terminal
 winget install Microsoft.WindowsTerminal
 
@@ -18,15 +21,18 @@ winget install Microsoft.WindowsTerminal
 winget install -e --id Microsoft.OneDrive
 # winget install 9NGHP3DX8HDX # Files-Community.Files 
 winget install voidtools.Everything
+winget install Dropbox.Dropbox
 
 # CUI Dev Tools
 winget install Git.Git
 winget install LLVM
 
-# GUI Dev Tools
+# Editor
 winget install Vim.Vim
 winget install Neovim.Neovim
 winget install -q vscode --scope machine # zsh setup/vscode.sh
+
+# GUI Dev Tools
 winget install Microsoft.VisualStudio.2022.Community
 winget install Postman.Postman
 winget install Arduino
@@ -45,17 +51,22 @@ winget install Discord.Discord
 winget install Line.Line
 winget install Telegram.TelegramDesktop
 winget install Tencent.WeChat
+# Snapchat, Skype, Chatwork, Teams
+
+# Language
 winget install Grammarly.ForWindows
 winget install -e --id DeepL.DeepL
-# Snapchat, Skype, Chatwork
 
-# GUI
+# GUI Behavior
 winget install rocksdanister.LivelyWallpaper
 winget install 9PF4KZ2VN4W9 # TranslucentTB
 
-# Others
-winget install -e --id NirSoft.BlueScreenView
-winget install Amazon.Kindle
-winget install Dropbox.Dropbox
+# Graphics
 winget install Gimp.Gimp
 winget install Adobe.Acrobat.Reader.64-bit
+
+# Book
+winget install Amazon.Kindle
+
+# Others
+winget install -e --id NirSoft.BlueScreenView
