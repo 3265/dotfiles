ab6d3f81799027755fb790f8b9b5981f8dd2faf5
mgoldchild <mgoldchild@gmail.com>
2022-11-13 11:25:01 +0900

    update

---
+++ .config/xmodmap/xmonad.hs
@@ -0,0 +1,19 @@
+import System.IO
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+main = do
+    xmproc <- spawnPipe "xmobar"
+    xmonad $ def
+        { manageHook = manageDocks <+> manageHook def
+        , layoutHook = avoidStruts $ layoutHook def
+        , logHook = dynamicLogWithPP $ xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , terminal           = "gnome-terminal"
+        , borderWidth        = 2
+        , normalBorderColor  = "#333333"
+        , focusedBorderColor = "#cd8b00"
+        }
