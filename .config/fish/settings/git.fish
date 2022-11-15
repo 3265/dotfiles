70ef0afbe3e0de31c10c10f81e2b3fee2b6397fe
mgoldchild <mgoldchild@gmail.com>
2022-11-16 00:21:30 +0900

    update

--- .config/fish/settings/git.fish
+++ .config/fish/settings/git.fish
@@ -1,4 +1,30 @@
-function gc -d "Git checkout branch"
+# ######################
+#   file
+# ######################
+
+function ga -d "Git add files to stage"
+  # ref: https://stackoverflow.com/questions/5237605/how-can-i-run-git-status-and-just-get-the-filenames
+  set file_name (git status --porcelain | sed s/^...// | fzf --preview="cat {}" )
+  git add $file_name
+end
+
+function gb -d "Git back; checkout modified files or reset staged files"
+  set file_name (git status --porcelain | sed s/^...// | fzf)
+  git checkout $file_name # for staged tracked files
+  git reset $file_name # for staged untrached files
+end
+
+function gc -d "Git commit with message"
+  set message $argv[1]
+  git commit -m $message
+end
+
+
+# ######################
+#   branch
+# ######################
+
+function gw -d "Git checkout branch"
   set branch (git for-each-ref --format='%(refname:short)' refs/heads | fzf)
   git checkout $branch
 end
@@ -11,10 +37,4 @@ end
 function gu -d "Git pull branch to origin server"
   set branch (git for-each-ref --format='%(refname:short)' refs/heads | fzf)
   git pull origin $branch
-end
-
-function ga -d "Git add files to stage"
-  # ref: https://stackoverflow.com/questions/5237605/how-can-i-run-git-status-and-just-get-the-filenames
-  set file_name (git status --porcelain | sed s/^...// | fzf --preview="cat {}" )
-  git add $file_name
 end
