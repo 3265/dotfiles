1026ccac1477d96e81e4a7e05c0476e4bb6fb3cd
mgoldchild <mgoldchild@gmail.com>
2022-12-20 22:21:39 +0900

    update

---
+++ aws.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# https://docs.aws.amazon.com/ja_jp/cli/latest/userguide/getting-started-install.html
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
