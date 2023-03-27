a0206b3934e2f880379a3f847f8fcae42c1ead46
3265 <mgoldchild@gmail.com>
2023-03-28 01:32:38 +0900

    chore

--- setup/deb/cui.sh
+++ setup/deb/cui.sh
@@ -24,6 +24,7 @@ sudo apt install -y qalc
 sudo apt install -y python3-pip
 sudo apt install -y python3-venv # needed by pipx
 sudo apt install -y python-is-python3
+sudo apt install -y nodejs
 sudo apt install -y default-jdk
 sudo apt install -y golang
 sudo apt install -y rust-all
