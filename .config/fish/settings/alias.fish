a22de7e91cf17f7e5f94aaf8eca01de9d8937de4
3265 <mgoldchild@gmail.com>
2024-05-15 22:47:18 +0900

    update

--- .config/fish/settings/alias.fish
+++ .config/fish/settings/alias.fish
@@ -1,22 +1,19 @@
 #!/bin/fish
 
 # Command shortcut
-alias ai="chatgpt --model=gpt4"
 alias a="ag --hidden --ignore .git"
 alias b="bash"
 alias c="code"
 alias d='docker'
 alias dc="docker-compose"
 alias e='/mnt/c/Windows/explorer.exe'
-function searchfile
-    find . -not -path './node_modules*' \
-        -a -not -path '*.git*' \
-        -iname "*"$argv"*"
-end
-alias f=searchfile
+alias f=search_file
+alias f-count=count_files
+alias f-stats=extension_stats
 alias h='history'
 alias p='pwd'
 alias g='git'
+alias i=runi
 alias k='kill'
 alias l='runls'
 alias m='make'
@@ -46,89 +43,6 @@ alias py='python' # install python-is-python3
 alias pe='pyenv' # python environment
 alias pm='poetry' # python manamger
 alias pa='source .venv/bin/activate.fish'
-
-function runls
-  if type -q "lsd"
-      lsd -la --group-dirs=first $argv
-   else
-      LC_COLLATE=C ls -al --color=auto --human-readable --group-directories-first $argv
-  end
-end
-
-function joshuto_cd
-    joshuto --output-file /tmp/joshutodir
-    set LASTDIR (cat /tmp/joshutodir)
-    cd "$LASTDIR"
-    commandline -f repaint
-    clear
-end
-
-function runi
-    set _branch $(git symbolic-ref --short HEAD 2> /dev/null)
-    set _stash_list $(git stash list 2> /dev/null | wc -l)
-    set _untracked $(git ls-files --others --exclude-standard 2> /dev/null | wc -l)
-    set _modified $(git status   2> /dev/null | grep 'modified:' | wc -l)
-    set _head $(git rev-parse --short HEAD 2> /dev/null)
-    set _last_message $(git show -s --format=%s 2> /dev/null)
-    set _commit_date $(git log -1 --format='%cd' 2> /dev/null)
-    set _acs $(stat -c '%a (%A)' .)
-    set _uid $(stat -c '%U (%u)' .)
-    set _gid $(stat -c '%G (%g)' .)
-
-    echo "- Machine:
-  - Username: $(whoami)
-  - Hostname: $(hostname)
-- File:
-  - Path : $(pwd)
-  - Access: $_acs
-  - Uid: $_uid
-  - Gid: $_gid
-- Commit:
-  - Hash: $_head
-  - Date: $_commit_date
-  - Message: $_last_message
-  - Branch: $_branch
-- Status:
-  - Modified : $_modified
-  - Untracked: $_untracked
-  - Stashed: $_stash_list
-- Venv:
-  - .venv: $(echo $VIRTUAL_ENV)"
-end
-
-function runpath
-    echo "- Path
-  - C: $(which c)
-  - C++: $(which c++)
-  - Python: $(which python)
-  - Go: $(which go)
-  - Java: $(which java)
-  - Node: $(which node)"
-end
-
-function ranger_cd
-    set -l tempfile (mktemp)
-    set -q argv[1]; and set dir_initial $argv[1]; or set dir_initial (pwd)
-    ranger --choosedir="$tempfile" "$dir_initial"
-    if test -f "$tempfile"
-        set -l dir (cat "$tempfile")
-        rm -f "$tempfile"
-        cd -- "$dir"
-        commandline -f repaint
-    end
-end
-
-function codemod
-  set target_name $argv[1]
-  set new_name $argv[2]
-
-  if test -z "$target_name" -o -z "$new_name"
-    echo "Both target_name and new_name are required."
-    return 1
-  end
-
-  fastmod -m --extensions yaml,ts,tsx,yaml,yml,json,toml,js,py,css,Dockerfile,env,md,html,conf,template,properties,dart $target_name $new_name
-end
 
 # Util
 alias cat='bat --style=plain'
@@ -160,8 +74,6 @@ alias egrep='egrep --color=auto'
 alias less='less -R'
 alias gitwatch='fswatch -0 . | xargs -0 -n 1 sh -c "clear && git status --short"' # to need to install fswatch
 
-# Others
-
 # Shell
 alias bsh='bash'
 alias fsh='fish'
@@ -188,3 +100,39 @@ alias shutdown='shutdown -h now'
 alias suspend='systemctl suspend'
 alias search='chromium-browser --app="https://google.com/"'
 
+# my funcitons
+function runls
+  if type -q "lsd"
+      lsd -la --group-dirs=first $argv
+   else
+      LC_COLLATE=C ls -al --color=auto --human-readable --group-directories-first $argv
+  end
+end
+
+# find
+function search_file
+    set name $argv
+    find . -not -path './node_modules*' -a -not -path '*.git*' -iname "*$name*"
+end
+
+function count_files
+    set dir $argv[1]
+    find $dir -type d \( -name node_modules -o -name .git \) -prune -o -type f -print | wc -l
+end
+
+function extension_stats
+    set dir $argv[1]
+    find $dir -type f | awk -F. '{if (NF>1) print $NF}' | sort | uniq -c | sort -nr
+end
+
+function codemod
+  set target_name $argv[1]
+  set new_name $argv[2]
+
+  if test -z "$target_name" -o -z "$new_name"
+    echo "Both target_name and new_name are required."
+    return 1
+  end
+
+  fastmod -m --extensions yaml,ts,tsx,yaml,yml,json,toml,js,py,css,Dockerfile,env,md,html,conf,template,properties,dart $target_name $new_name
+end
