b2d44853052ce2304d3e6e7963cf8acd926e7f73
3265 <mgoldchild@gmail.com>
2024-08-31 12:45:31 +0900

    update prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,127 +1,74 @@
 set -g fish_prompt_pwd_dir_length 4
 
+# プロンプトモードを制御する変数（0: シンプル、1: 詳細）
+set -g PROMPT_MODE 1
+
+
 function fish_prompt
-    echo -n '▶ '
-end 
+    set -l last_status $status
 
-# トップライン表示を制御する環境変数
-set -g SHOW_TOP_LINE 1
-
-# function fish_right_prompt
-#     prompt_pwd
-# end
-
-function fish_update_top_line --on-variable PWD --on-variable fish_bind_mode --on-event fish_postexec
-    if test "$SHOW_TOP_LINE" = "0"
-        return
-    end
-
-    # git info
-    set -l git_branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
-    set -l git_info ""
-    if test -n "$git_branch"
-        set git_info "($git_branch)"
-    end
-
-    # Python version and VENV info
-    set -l python_version (python --version 2>&1 | cut -d' ' -f2)
-    set -l python_info "py=$python_version"
-    set -l venv_info ""
-    if set -q VIRTUAL_ENV
-        set venv_info " (venv="(basename "$VIRTUAL_ENV")")"
-    end
-
-	# Node.js version (using nvm)
-    set -l node_version ""
-    if type -q nvm
-        set node_version (nvm current 2>/dev/null)
-        if test -n "$node_version"
-            set node_version "node=$node_version"
+    if test "$PROMPT_MODE" = "1"
+        # 詳細モード（2行）
+        # First line
+        set -l prompt_info
+        # プロンプト情報の作成（git, Python, Node.js）
+        set -l git_branch (git rev-parse --abbrev-ref HEAD 2>/dev/null)
+        if test -n "$git_branch"
+            set -a prompt_info (set_color yellow)"($git_branch)"(set_color normal)
         end
+
+        set -l python_version (python --version 2>&1 | cut -d' ' -f2)
+        set -a prompt_info (set_color green)"py=$python_version"(set_color normal)
+
+        if set -q VIRTUAL_ENV
+            set -a prompt_info (set_color green)"(venv="(basename "$VIRTUAL_ENV")")"(set_color normal)
+        end
+
+        if type -q nvm
+            set -l node_version (nvm current 2>/dev/null)
+            if test -n "$node_version"
+                set -a prompt_info (set_color magenta)"node=$node_version"(set_color normal)
+            end
+        end
+
+        echo -n (set_color yellow)"┌──"(set_color normal)" "
+        echo -n (set_color blue)(prompt_pwd)(set_color normal)
+        for info in $prompt_info
+            echo -n " $info"
+        end
+        echo
+        echo -n (set_color yellow)"└─▶"(set_color normal)" "
+    else
+        # Second line (シンプルモードの場合はこれだけ表示)
+        echo -n (set_color yellow)"▶"(set_color normal)" "
     end
-
-
-    set -l top_line (set_color cyan)"●"\
-                    (set_color blue)(prompt_pwd)\
-                    (set_color yellow)$git_info \
-                    (set_color green)$python_info$venv_info\
-                    (set_color magenta)$node_version\
-                    (set_color normal)
-    
-    # カーソルを一番上の行に移動
-    echo -en '\e[s\e[H\e[K'
-    
-    # 情報を表示
-    echo -n $top_line
-    
-    # カーソルを元の位置に戻す
-    echo -en '\e[u'
 end
 
-# キー入力のたびに更新
-function fish_user_key_bindings
-    for mode in insert default visual
-        bind -M $mode -m default \r 'commandline -f execute; fish_update_top_line'
+function toggle_prompt_mode
+    if test "$PROMPT_MODE" = "1"
+        set -g PROMPT_MODE 0
+        clear2
+    else
+        set -g PROMPT_MODE 1
+        clear2
     end
+    commandline -f repaint
 end
 
-# プロンプト表示直前にトップラインを更新
-function fish_prompt_update_topline --on-event fish_prompt
-    fish_update_top_line
-end
-
-# ターミナルのリサイズ時にも更新
-function on_resize --on-signal WINCH
-    fish_update_top_line
-end
 
 function clear2
-    # clear # NOE: こっちだと、ターミナルをスクロールアップすると履歴が0になってしまう
     echo -en "\033c"
 end
 
-function clear_top_line
-    clear2
-    commandline -f repaint
-    fish_update_top_line
-    echo -en '\e[1E' # カーソルを次の行の先頭に移動
-    fish_prompt
-    echo -en '\e[2;3H' # カーソルを▶の直後に移動
-end
+# プロンプトモードを切り替えるためのキーバインド
+bind \ct 'toggle_prompt_mode'
 
-# トップライン表示を切り替える関数
-function toggle_top_line
-    if test "$SHOW_TOP_LINE" = "1"
-        set -g SHOW_TOP_LINE 0
-        fish_clear
-    else
-        set -g SHOW_TOP_LINE 1
-        fish_clear
-    end
-end
-
-# トップライン表示を切り替えるためのキーバインド（Ctrl+T）
-bind \ct 'toggle_top_line'
-
-function fish_clear
-    if test "$SHOW_TOP_LINE" = "1"
-        clear_top_line
-    else
-        clear2
-        commandline -f repaint
-        fish_prompt
-    end
-end
-
-# ctrl+l に新しい関数をバインド
-bind \cl 'fish_clear'
-
-# 初回起動時にトップラインを表示し、自動的にEnterを押す
+# 初回起動時に詳細モードで開始
 if status --is-interactive
-    function __auto_enter --on-event fish_prompt
-        if not set -q __fish_auto_enter_done
-            set -g __fish_auto_enter_done 1
-            commandline -f execute
+    function __initial_prompt_mode --on-event fish_prompt
+        if not set -q __fish_initial_mode_set
+            set -g __fish_initial_mode_set 1
+            set -g PROMPT_MODE 1
         end
     end
 end
