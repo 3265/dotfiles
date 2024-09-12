b83e5934a60ec7f3352baa3ca168273ba810a686
3265 <mgoldchild@gmail.com>
2024-09-12 13:34:22 +0900

    update

--- setup/linux/tmux.sh
+++ setup/linux/tmux.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+TPM_DIR="$HOME/.tmux/plugins/tpm"
+
+
+if [ -d "$TPM_DIR" ]; then
+    echo "Existing TPM directory found. Removing it..."
+    rm -rf "$TPM_DIR"
+fi
+
 mkdir -p ~/.tmux/plugins
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm $TPM_DIR
+
 # Install plugins using command(C-I) on Tmux
 # And Reload Plugins using command(C-r) on Tmux
 # https://github.com/tmux-plugins/tpm
