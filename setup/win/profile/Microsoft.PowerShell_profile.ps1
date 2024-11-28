bdc366888bd6fd9cdb6fa81710a1e606ba38e54a
3265 <mgoldchild@gmail.com>
2024-11-28 20:22:06 +0900

    win profile

---
+++ setup/win/profile/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,9 @@
+# NOTE:
+# - run $profile for file location
+# - run below in powershell as admin
+#   - Set-ExecutionPolicy RemoteSigned
+
+# vim alias
+set-alias vi 'C:\Program Files\Vim\Vim91\vim.exe'
+set-alias vim 'C:\Program Files\Vim\Vim91\vim.exe'
+
