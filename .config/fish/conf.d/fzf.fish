389c34ab8fb82898cdb732a745d03c57cbd241e4
3265 <mgoldchild@gmail.com>
2025-03-27 20:31:20 +0900

    update

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -1,9 +1,3 @@
-# Ref:
-#  - https://github.com/jethrokuan/fzf
-#  - https://github.com/PatrickF1/fzf.fish
-
-# bind \cs '__fzf_find_file'
-# bind \cd '__fzf_find_dir'
 bind \cd '__fzf_cd'
 bind \cf '__fzf_open'
 bind \cr '__fzf_autojump'
