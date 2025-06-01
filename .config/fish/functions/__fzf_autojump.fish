9439701bb941bcc36a01cb5ab23df162f81d9bed
3265 <mgoldchild@gmail.com>
2025-06-02 02:26:03 +0900

    update

--- .config/fish/functions/__fzf_autojump.fish
+++ .config/fish/functions/__fzf_autojump.fish
@@ -9,16 +9,24 @@ function __fzf_autojump -d "auto jump"
     end
 
     # autojump のパス一覧を fzf で選択
+    # 古いパスはCtrl-Dで消す
     set -l select (autojump -s | sort -k1gr \
         | awk '$1 ~ /[0-9]:/ && $2 ~ /^\// { for (i=2; i<=NF; i++) { print $(i) } }' \
         | fzf --prompt 'JumpToRecentDirectory>' \
               --preview-window='bottom:3:wrap' \
               --preview='echo {}' \
+              --bind 'ctrl-d:execute(autojump --purge && echo "[autojump] Purged nonexistent paths." >&2)' \
+              --expect=enter \
               +m)
 
-    if test -n "$select"
-        builtin cd "$select"
-        commandline -t ""
+    if test (count $select) -gt 1
+        set -l key $select[1]
+        set -l path $select[2]
+
+        if test "$key" = enter -a -d "$path"
+            builtin cd "$path"
+            commandline -t ""
+        end
     end
 
     commandline -f repaint
