6bf99668a5ba914aca6c08a38c555b5538bbb7e8
3265 <8959761+3265@users.noreply.github.com>
2026-04-21 20:05:43 +0900

    vnc

--- deb/scripts/tigervnc-server.sh
+++ deb/scripts/tigervnc-server.sh
@@ -2,10 +2,20 @@
 
 # sudo apt install tigervnc-scraping-server
 # sudo apt install tigervnc-standalone-server
+# sudo apt install ufw
+
+sudo ufw enable
+sudo ufw allow 5900
+sudo ufw status
 
 ip -4 address
 
-pkill x0tigervncserver
-xauth merge /run/user/1000/gdm/Xauthority
-x0tigervncserver -display :1 -rfbport 5900 -SecurityTypes None
+pkill -9 -f x0tigervncserver
+pkill -9 -f x0tigervnc
 
+sleep 1
+
+#xauth merge /run/user/1000/gdm/Xauthority
+x0tigervncserver -display :1 -rfbport 5900 -SecurityTypes None -localhost=0 --I-KNOW-THIS-IS-INSECURE
+
+x0tigervncserver -list
