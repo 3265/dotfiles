654a833ab45562a62ca8a4924ac02cf0c4241e1a
3265 <8959761+3265@users.noreply.github.com>
2026-05-09 14:28:19 +0900

    LAN

--- deb/sys/ssh-pw.sh
+++ deb/sys/ssh-pw.sh
@@ -5,8 +5,6 @@ set -e
 sudo apt install -y openssh-server
 
 sudo tee /etc/ssh/sshd_config.d/00-localhost-password.conf >/dev/null <<'EOF'
-ListenAddress 127.0.0.1
-ListenAddress ::1
 PasswordAuthentication yes
 KbdInteractiveAuthentication yes
 UsePAM yes
@@ -19,3 +17,4 @@ sudo systemctl restart ssh
 
 echo "Check:"
 sudo sshd -T | grep -Ei '^(listenaddress|passwordauthentication|kbdinteractiveauthentication|usepam|permitrootlogin)'
+sudo systemctl restart sshd
