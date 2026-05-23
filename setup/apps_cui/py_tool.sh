34e2c56df707eb68cba6d0dcc82cf5d1c26d5b39
3265 <8959761+3265@users.noreply.github.com>
2026-05-23 18:02:40 +0900

    update

--- setup/apps_cui/py_tool.sh
+++ setup/apps_cui/py_tool.sh
@@ -5,10 +5,10 @@ sudo apt install -y \
   python3-pip \
   python3-venv \
   python3-poetry \
-  python3-black \
   python3-mypy \
   python3-pytest \
   python3-ipython \
   python3-flake8 
   #pgcli
 
+python -m pip install -U huggingface_hub black
