00d819a5426dff21149a1a79d933a7a662374397
3265 <8959761+3265@users.noreply.github.com>
2026-06-06 19:38:43 +0900

    update

--- setup/service.sh
+++ setup/service.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-sudo cp "/home/mike/dotfiles/setup/system/nvidia-power-limit.service" /etc/systemd/system/
+sudo cp "/home/mike/dotfiles/setup/systemd/nvidia-power-limit.service" /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable --now nvidia-power-limit.service
