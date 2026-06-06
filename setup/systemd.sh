e45d34dd574035ef21d3b54935dcc5b8d0170741
3265 <8959761+3265@users.noreply.github.com>
2026-06-06 18:19:56 +0900

    bugfix

--- setup/systemd.sh
+++ setup/systemd.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -e
 
-DOTFILES="$(cd "$(dirname "$0")/../.." && pwd)"
-
-sudo cp "$DOTFILES/setup/system/nvidia-power-limit.service" /etc/systemd/system/
+sudo cp "/home/mike/dotfiles/setup/system/nvidia-power-limit.service" /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable --now nvidia-power-limit.service
